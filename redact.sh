977d6a6fa6e934bc75c1d77110d3bdd5fe4969c2
Anthony Vandewerdt <34634682+aussiestorblog@users.noreply.github.com>
2020-05-19 08:29:46 +1000

    Update redact.sh

--- redact.sh
+++ redact.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-# Edit this line to change the sql file name if needed.   SID and orahome should be detected automatically
+# You don't need to set any variables unless you are manually running this script
+# ORACLE_SID is learned from env variable databasesid that exists during mount, but not manual test
+# ORACLE_HOME is learned from env variable orahome that exists only during certain parts of mount, but not manual test
+# sqlscriptname is learned by $2 passed to script during mount, but not manual test
+
+#  if $2 is not set then the sql script name will be empty
+if [ -z "$2" ]; then
+	echo "The $2 variable which defines the SQL script to be run was not passed to the script"
+	echo "In a workflow you need to put the SQL file name after the script name"
+else
+	sqlscriptname=$(echo "$2")
+fi
+
 maskfunc()
 {
-script=redact.sql
 ORACLE_HOME=$orahome
 ORACLE_SID=$databasesid
-oraclecommand="cd /act/scripts;export ORACLE_HOME=$ORACLE_HOME;export ORACLE_SID=$ORACLE_SID;export PATH=$ORACLE_HOME/bin:$PATH;ORAENV_ASK=NO;sqlplus / as sysdba @/act/scripts/$script;exit"
+oraclecommand="cd /act/scripts;export ORACLE_HOME=$ORACLE_HOME;export ORACLE_SID=$ORACLE_SID;export PATH=$ORACLE_HOME/bin:$PATH;ORAENV_ASK=NO;sqlplus / as sysdba @/act/scripts/$sqlscriptname;exit"
 echo "$oraclecommand"
 su -m oracle -c "$oraclecommand"
 }
+
 
 # test for testing
 if [ -z "$1" ]; then
